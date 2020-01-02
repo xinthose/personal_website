988104a675af5b4927cc261d8beccad377a98392
xinthose <adamdunsmoor@pm.me>
2020-01-01 21:55:43 -0600

    remove the grid; changing gears

--- src/app/bible.service.ts
+++ src/app/bible.service.ts
@@ -2,12 +2,6 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient } from "@angular/common/http";
 
-// Progress
-import { GridDataResult } from '@progress/kendo-angular-grid';
-import { toODataString } from '@progress/kendo-data-query';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -26,52 +20,5 @@ export class BibleService {
   }
   public fetchVerses() {
     return this.http.get<Array<{ verseName: string, verseId: number, chapterId: number, bookId: number }>>('./assets/bible/verses.json');
-  }
-}
-
-export abstract class BibleServiceGrid extends BehaviorSubject<GridDataResult> {
-  public loading: boolean;
-
-  private BASE_URL = './assets/bible/verses.json';
-
-  constructor(
-    private http: HttpClient,
-    protected tableName: string
-  ) {
-    super(null);
-  }
-
-  public query(state: any): void {
-    this.fetch(this.tableName, state)
-      .subscribe(x => super.next(x));
-  }
-
-  protected fetch(tableName: string, state: any): Observable<GridDataResult> {
-    this.loading = true;
-
-    return this.http
-      .get(this.BASE_URL)
-      .pipe(
-        map(response => (<GridDataResult>{
-          data: response['value'],
-          total: parseInt(response['@odata.count'], 10)
-        })),
-        tap(() => this.loading = false)
-      );
-  }
-}
-
-@Injectable({
-  providedIn: 'root'
-})
-export class BibleServiceGrid1 extends BibleServiceGrid {
-  constructor(http: HttpClient) { super(http, 'Categories'); }
-
-  queryAll(st?: any): Observable<GridDataResult> {
-    const state = Object.assign({}, st);
-    delete state.skip;
-    delete state.take;
-
-    return this.fetch(this.tableName, state);
   }
 }
--- src/app/bible/bible.component.ts
+++ src/app/bible/bible.component.ts
@@ -2,12 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 // Progress
 import { GroupResult, groupBy } from '@progress/kendo-data-query';
-import { GridDataResult, DataStateChangeEvent } from '@progress/kendo-angular-grid';
 import { Observable } from 'rxjs';
-import { State } from '@progress/kendo-data-query';
 
 import { BibleService } from "../bible.service";
-import { BibleServiceGrid1 } from "../bible.service";
 
 @Component({
   selector: 'app-bible',
@@ -48,22 +45,10 @@ export class BibleComponent {
   selectedVerseStart: { verseName: string, verseId: number };
   selectedVerseEnd: { verseName: string, verseId: number };
 
-  // Bible Grid
-  public view: Observable <GridDataResult>;
-  public gridData: any;
-
   constructor(
     private bibleService: BibleService,
-    private bibleServiceGrid1: BibleServiceGrid1,
   ) {
-    // fetch JSON data asynchronously 
-    this.view = bibleServiceGrid1;
-    const state: State = {};
-    this.bibleServiceGrid1.query(state);
-    this.view.subscribe(res => {
-      this.gridData = res;
-      });
-
+    // fetch JSON data asynchronously
     this.bibleService.fetchBooks()
       .subscribe(response => {
         if (this.debug) console.debug("response = " + JSON.stringify(response));
