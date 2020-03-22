fd5928e603c7f664eeb9ddfbaabb79566cc74d17
xinthose <adamdunsmoor@pm.me>
2020-03-22 00:54:44 -0500

    remove package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 
-// Firebase
-import * as firebase from "firebase/app";
-
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    
+
     constructor() {
-        
+
     }
 }
