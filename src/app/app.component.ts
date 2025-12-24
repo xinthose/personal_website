de08e9811e251777f9d27bd9c0460fd1616476bc
Adam Dunsmoor <adamdunsmoor@pm.me>
2025-12-24 11:42:12 -0600

    updates for angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,9 @@ import { faBookBible, faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 import { Subscription } from "rxjs";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnDestroy {
   public logID: string = "AppComponent.";
